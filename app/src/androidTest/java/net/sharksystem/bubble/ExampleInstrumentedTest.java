4d93100e85eb4bc3c67081c691b39cca3223d3e0
thsc42 <thomas.schwotzer@htw-berlin.de>
2019-02-14 19:28:32 +0100

    it compiles and is now on github. Let's go :)

--- app/src/androidTest/java/net/sharksystem/bubble/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/net/sharksystem/bubble/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("ohdm.de.temp", appContext.getPackageName());
+        assertEquals("net.sharksystem", appContext.getPackageName());
     }
 }
