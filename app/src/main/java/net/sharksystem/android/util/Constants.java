28311752e83a9bb4b731113725181193db2e5957
pascal-dettmers <pascal.dettmers@googlemail.com>
2019-04-19 17:49:51 +0200

    add icons for nav drawer

--- app/src/main/java/net/sharksystem/android/util/Constants.java
+++ app/src/main/java/net/sharksystem/android/util/Constants.java
@@ -8,6 +8,5 @@ public class Constants {
 
     public static final String KEY_ALIAS_USER = "keyAliasUser";
     public static final String UUID_USER = "uuidUser";
-    public static final String ALGORITHM_USED_FOR_SIGNING = "SHA256withRSA/PSS";
     public static final String FILENAME = "SharkNet2PreferenceFilename";
 }
