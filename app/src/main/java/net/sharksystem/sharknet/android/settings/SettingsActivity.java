ea8286756a8bb8dab22e5cba2e30bc9fb06af90f
thsc42 <thomas.schwotzer@htw-berlin.de>
2019-09-10 12:48:59 +0200

    still as sync problem in settings activity with bt discoverable button - rest pretty stable

--- app/src/main/java/net/sharksystem/sharknet/android/settings/SettingsActivity.java
+++ app/src/main/java/net/sharksystem/sharknet/android/settings/SettingsActivity.java
@@ -42,12 +42,14 @@ public class SettingsActivity extends SharkNetActivity {
                 new CompoundButton.OnCheckedChangeListener() {
                     @Override
                     public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                        if (isChecked) {
-                            Log.d(getLogStart(), "ui said: switch on BT");
-                            startBluetooth();
-                        } else {
-                            Log.d(getLogStart(), "ui said: switch off BT");
-                            stopBluetooth();
+                        if(!dontDoAnything) {
+                            if (isChecked) {
+                                Log.d(getLogStart(), "ui said: switch on BT");
+                                startBluetooth();
+                            } else {
+                                Log.d(getLogStart(), "ui said: switch off BT");
+                                stopBluetooth();
+                            }
                         }
                     }
                 });
@@ -62,12 +64,14 @@ public class SettingsActivity extends SharkNetActivity {
                 new CompoundButton.OnCheckedChangeListener() {
                     @Override
                     public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                        if (isChecked) {
-                            Log.d(getLogStart(), "ui said: switch on BT Discoverable");
-                            startBluetoothDiscoverable();
-                        } else {
-                            Log.d(getLogStart(), "ui said: switch off BT - there is no switch off - yet TODO");
-                            stopBluetooth();
+                        if(!dontDoAnything) {
+                            if (isChecked) {
+                                Log.d(getLogStart(), "ui said: switch on BT Discoverable");
+                                startBluetoothDiscoverable();
+                            } else {
+                                Log.d(getLogStart(), "ui said: switch off BT - there is no switch off - yet TODO");
+                                stopBluetooth();
+                            }
                         }
                     }
                 });
@@ -82,14 +86,81 @@ public class SettingsActivity extends SharkNetActivity {
                 new CompoundButton.OnCheckedChangeListener() {
                     @Override
                     public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                        if (isChecked) {
-                            Log.d(getLogStart(), "ui said: switch on BT Discovery");
-                            startBluetoothDiscovery();
-                        } else {
-                            Log.d(getLogStart(), "ui said: switch off BT - TODO nyi");
-                            //sharkNetApp.stopBluetooth();
+                        if (!dontDoAnything) {
+                            if (isChecked) {
+                                Log.d(getLogStart(), "ui said: switch on BT Discovery");
+                                startBluetoothDiscovery();
+                            } else {
+                                Log.d(getLogStart(), "ui said: switch off BT - TODO nyi");
+                                //sharkNetApp.stopBluetooth();
+                            }
                         }
                     }
                 });
+    }
+
+    private boolean dontDoAnything = false;
+
+    private void refreshToggleButtons() {
+        this.dontDoAnything = true;
+        ToggleButton toggle = (ToggleButton) findViewById(R.id.settingsBluetoothToggleButton);
+        toggle.setChecked(this.isBluetoothEnvironmentOn());
+        toggle.refreshDrawableState();
+
+        toggle = (ToggleButton) findViewById(R.id.settingsBluetoothDiscoverableToggleButton);
+        toggle.setChecked(this.isBluetoothDiscoverable());
+        toggle.refreshDrawableState();
+
+        toggle = (ToggleButton) findViewById(R.id.settingsBluetoothDiscoveryToggleButton);
+        toggle.setChecked(this.isBluetoothDiscovery());
+        toggle.refreshDrawableState();
+        this.dontDoAnything = false;
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        this.refreshProtocolStatus();
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////
+    //                             keep in sync with protocol changes                         //
+    ////////////////////////////////////////////////////////////////////////////////////////////
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        this.refreshProtocolStatus();
+    }
+
+    public void aspNotifyBTDiscoverableStarted() {
+        super.aspNotifyBTDiscoverableStarted();
+        this.refreshToggleButtons();
+    }
+
+    public void asapNotifyBTDiscoverableStopped() {
+        super.asapNotifyBTDiscoverableStopped();
+        this.refreshToggleButtons();
+    }
+
+    public void aspNotifyBTEnvironmentStarted() {
+        super.aspNotifyBTEnvironmentStarted();
+        this.refreshToggleButtons();
+    }
+
+    public void aspNotifyBTEnvironmentStopped() {
+        super.aspNotifyBTEnvironmentStopped();
+        this.refreshToggleButtons();
+    }
+
+    public void aspNotifyBTDiscoveryStarted() {
+        super.aspNotifyBTDiscoveryStarted();
+        Log.d(this.getLogStart(), "aspNotifyBTDiscoveryStarted() called");
+        this.refreshToggleButtons();
+    }
+
+    public void aspNotifyBTDiscoveryStopped() {
+        super.aspNotifyBTDiscoveryStopped();
+        this.refreshToggleButtons();
     }
 }
