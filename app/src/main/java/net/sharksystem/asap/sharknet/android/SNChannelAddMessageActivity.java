71211d11987c712b9b990b7025492dacbf6dcfd9
thsc42 <thomas.schwotzer@htw-berlin.de>
2020-09-22 08:23:03 +0200

    ready to start debugging sn channels - towards a working version

--- app/src/main/java/net/sharksystem/asap/sharknet/android/SNChannelAddMessageActivity.java
+++ app/src/main/java/net/sharksystem/asap/sharknet/android/SNChannelAddMessageActivity.java
@@ -1,8 +1,10 @@
 package net.sharksystem.asap.sharknet.android;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -13,6 +15,8 @@ import net.sharksystem.android.ASAPChannelIntent;
 import net.sharksystem.asap.ASAPException;
 import net.sharksystem.asap.sharknet.InMemoSNMessage;
 import net.sharksystem.crypto.BasicKeyStore;
+import net.sharksystem.persons.android.PersonListSelectionActivity;
+import net.sharksystem.persons.android.PersonsStorageAndroidComponent;
 
 import java.io.IOException;
 import java.util.HashSet;
@@ -21,6 +25,7 @@ import java.util.Set;
 public class SNChannelAddMessageActivity extends SNChannelsActivity {
     private CharSequence name = null;
     private CharSequence uri;
+    private Set<CharSequence> selectedRecipients = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,7 +52,46 @@ public class SNChannelAddMessageActivity extends SNChannelsActivity {
         topicTextView.setText(this.name);
     }
 
-    public void onAddClick(View view) {
+    public void onSelectRecipients(View view) {
+        PersonsStorageAndroidComponent.getPersonsStorage().setPreselectionSet(this.selectedRecipients);
+        //Log.d(this.getLogStart(), "setPreselected: " + this.selectedRecipients);
+        Intent intent = new Intent(this, PersonListSelectionActivity.class);
+        this.startActivity(intent);
+    }
+
+    public void onEncryptedClick(View view) {
+        this.redrawComments();
+    }
+
+    private void redrawComments() {
+        TextView commentsTextView = findViewById(R.id.comments);
+        commentsTextView.setText("");
+
+        CheckBox encryptedCheckBox = findViewById(R.id.snEncrypted);
+
+        if(this.selectedRecipients != null && this.selectedRecipients.size() > 1) {
+            if (encryptedCheckBox.isChecked()) {
+                commentsTextView.setText("each recipient gets its own encrypted message");
+            }
+        }
+
+        if(this.selectedRecipients == null || this.selectedRecipients.isEmpty()) {
+            if(encryptedCheckBox.isChecked()) {
+                encryptedCheckBox.setChecked(false);
+                Toast.makeText(this,
+                        "select recipient(s) - cannot encrypt message to anybody",
+                        Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    public void onRemoveRecipients(View view) {
+        this.selectedRecipients = null;
+        PersonsStorageAndroidComponent.getPersonsStorage().setPreselectionSet(null);
+        this.redrawRecipientList();
+    }
+
+    public void onSendClick(View view) {
         // get new message
         EditText messageTextView = (EditText) findViewById(R.id.snMessage);
 
@@ -57,28 +101,32 @@ public class SNChannelAddMessageActivity extends SNChannelsActivity {
             Toast.makeText(this, "message is empty", Toast.LENGTH_SHORT).show();
         } else {
             try {
-
                 // let's sort things out.
-                CharSequence sender =
-                        SNChannelsComponent.getSharkNetChannelComponent().getOwnerID();
-                Set<CharSequence> recipients = new HashSet<>();
-                recipients.add(sender); // TODO must come from GUI
-                boolean sign = true; // TODO must come from GUI
-                boolean encrypt = true;  // TODO must come from GUI
-                BasicKeyStore basicKeyStore =
-                        SNChannelsComponent.getSharkNetChannelComponent().getBasicKeyStore();
+                CheckBox signCheckBox = findViewById(R.id.snSigned);
+                boolean sign = signCheckBox.isChecked();
+
+                CheckBox encryptedCheckBox = findViewById(R.id.snEncrypted);
+                boolean encrypt = encryptedCheckBox.isChecked();
 
                 byte[] content = messageText.getBytes();
 
-                byte[] serializedMessage = null;
-                    serializedMessage = InMemoSNMessage.serializeMessage(
-                            content, sender, recipients, sign, encrypt, basicKeyStore);
-
-                // deliver as asap message
-                this.sendASAPMessage(SNChannelsComponent.APP_NAME, this.uri,
-                        serializedMessage, true);
-
+                // lets produce and send asap messages
+                // 1) no recipients
+                if(this.selectedRecipients == null || this.selectedRecipients.isEmpty()) {
+                    this.compileAndSendASAPMessage(content, null, sign, encrypt);
+                } else {
+                    // 2) no encryption or just a single recipient
+                    if(!encrypt || this.selectedRecipients.size() == 1) {
+                        this.compileAndSendASAPMessage(content, sign, encrypt);
+                    } else {
+                        // 3) encrypt more than one recipient - each gets its own message
+                        for(CharSequence receiver : this.selectedRecipients) {
+                            this.compileAndSendASAPMessage(content, receiver, sign, encrypt);
+                        }
+                    }
+                }
             } catch (ASAPException | IOException e) {
+                Toast.makeText(this, e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
                 Log.d(this.getLogStart(), "problems when sending message in SNChannel: "
                         + e.getLocalizedMessage());
             }
@@ -88,9 +136,77 @@ public class SNChannelAddMessageActivity extends SNChannelsActivity {
         this.finish();
     }
 
+    private void compileAndSendASAPMessage(byte[] content, boolean sign, boolean encrypt)
+            throws IOException, ASAPException {
+        CharSequence sender =
+                SNChannelsComponent.getSharkNetChannelComponent().getOwnerID();
+        BasicKeyStore basicKeyStore =
+                SNChannelsComponent.getSharkNetChannelComponent().getBasicKeyStore();
+        byte[] serializedMessage = InMemoSNMessage.serializeMessage(
+                content, sender, this.selectedRecipients, sign, encrypt, basicKeyStore);
+        // deliver as asap message
+        this.sendASAPMessage(SNChannelsComponent.APP_NAME, this.uri,
+                serializedMessage, true);
+    }
+
+    private void compileAndSendASAPMessage(byte[] content, CharSequence recipient,
+                           boolean sign, boolean encrypt)
+            throws IOException, ASAPException {
+        CharSequence sender =
+                SNChannelsComponent.getSharkNetChannelComponent().getOwnerID();
+        BasicKeyStore basicKeyStore =
+                SNChannelsComponent.getSharkNetChannelComponent().getBasicKeyStore();
+        byte[] serializedMessage = InMemoSNMessage.serializeMessage(
+                content, sender, recipient, sign, encrypt, basicKeyStore);
+        // deliver as asap message
+        this.sendASAPMessage(SNChannelsComponent.APP_NAME, this.uri,
+                serializedMessage, true);
+    }
+
     public void onAbortClick(View view) {
         ASAPChannelIntent intent = new ASAPChannelIntent(this, this.name, this.uri,
                 SNChannelViewActivity.class);
         startActivity(intent);
+    }
+
+    private void redrawRecipientList() {
+        String recipientsString = "anybody";
+
+        if(this.selectedRecipients != null && !this.selectedRecipients.isEmpty()) {
+            StringBuilder sb = new StringBuilder();
+            boolean first = true;
+            for(CharSequence recipient : this.selectedRecipients) {
+                CharSequence recipientName = recipient;
+                try {
+                    recipientName = SNChannelsComponent.getSharkNetChannelComponent().
+                            getPersonName(recipient);
+                } catch (ASAPException e) {
+                    // name not found
+                }
+
+                if(first) {
+                    first = false;
+                } else {
+                    sb.append(" | ");
+                }
+                sb.append(recipientName);
+            }
+            recipientsString = sb.toString();
+        }
+
+        TextView tv = this.findViewById(R.id.snMessageRecipients);
+        tv.setText(recipientsString);
+
+        this.redrawComments();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        this.selectedRecipients =
+                PersonsStorageAndroidComponent.getPersonsStorage().getLastPersonsSelection();
+
+        this.redrawRecipientList();
     }
 }
