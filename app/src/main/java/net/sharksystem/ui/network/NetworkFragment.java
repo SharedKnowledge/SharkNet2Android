f59f395e99d64bead21f4f1d91df7080f3c96410
Clemens <cle.zander@gmail.com>
2023-03-01 09:09:05 +0100

    added comments for developers

--- app/src/main/java/net/sharksystem/ui/network/NetworkFragment.java
+++ app/src/main/java/net/sharksystem/ui/network/NetworkFragment.java
@@ -1,38 +1,54 @@
 package net.sharksystem.ui.network;
 
-import androidx.lifecycle.ViewModelProvider;
-
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import net.sharksystem.R;
+import net.sharksystem.databinding.FragmentNetworkBinding;
+
 
 public class NetworkFragment extends Fragment {
 
-    private NetworkViewModel mViewModel;
-
-    public static NetworkFragment newInstance() {
-        return new NetworkFragment();
-    }
+    /**
+     * Binding to access elements from the layout
+     */
+    private FragmentNetworkBinding binding;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_network, container, false);
-    }
 
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = new ViewModelProvider(this).get(NetworkViewModel.class);
-        // TODO: Use the ViewModel
-    }
+        this.binding = FragmentNetworkBinding.inflate(inflater,container,false);
 
+        //Set-Up OnClickListeners
+        //...when bluetooth is switched on or off
+        this.binding.fragmentNetworkBluetoothOnOffButton.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            //TODO: activate or deactivate bluetooth
+        });
+
+        //...when bluetooth scanning should be enabled or disabled
+        this.binding.fragmentNetworkBluetoothScanButton.setOnCheckedChangeListener(((buttonView, isChecked) -> {
+            //TODO: make visible and scan or stop discovery and itself stop being discoverable
+        }));
+
+        //...when connections to hubs should be made or not
+        this.binding.fragmentNetworkBluetoothConnectHubsButton.setOnCheckedChangeListener(((buttonView, isChecked) -> {
+            //TODO: allow to connect to hubs or not
+        }));
+
+        //...when hubs are configured
+        this.binding.fragmentNetworkBluetoothConfigureHubsButton.setOnClickListener(view ->
+                Navigation.findNavController(view)
+                        .navigate(R.id.action_nav_network_to_nav_hub_list));
+
+        return this.binding.getRoot();
+    }
 }
--- app/src/main/java/net/sharksystem/ui/radar/RadarFragment.java
+++ app/src/main/java/net/sharksystem/ui/radar/RadarFragment.java
@@ -1,7 +1,5 @@
 package net.sharksystem.ui.radar;
 
-import androidx.lifecycle.ViewModelProvider;
-
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -12,27 +10,27 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import net.sharksystem.R;
+import net.sharksystem.databinding.FragmentRadarBinding;
 
+
+/**
+ * TODO: what's the purpose
+ */
 public class RadarFragment extends Fragment {
 
-    private RadarViewModel mViewModel;
-
-    public static RadarFragment newInstance() {
-        return new RadarFragment();
-    }
+    /**
+     * Binding to access elements from the layout
+     */
+    private FragmentRadarBinding binding;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_radar, container, false);
-    }
 
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = new ViewModelProvider(this).get(RadarViewModel.class);
-        // TODO: Use the ViewModel
-    }
+        //TODO: What is this view supposed to display? An OSM-Map based visualization of peers?
 
+        this.binding = FragmentRadarBinding.inflate(inflater, container, false);
+
+        return this.binding.getRoot();
+    }
 }
