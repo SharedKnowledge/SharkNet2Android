87b9d0033493f7724108a3608fc02540bd20dd43
Clemens <cle.zander@gmail.com>
2023-03-01 08:25:36 +0100

    ChannelViewFragment now displays added messages.

--- app/src/main/java/net/sharksystem/ui/channels/massage/ChannelViewFragment.java
+++ app/src/main/java/net/sharksystem/ui/channels/massage/ChannelViewFragment.java
@@ -2,20 +2,56 @@ package net.sharksystem.ui.channels.massage;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import net.sharksystem.R;
+import net.sharksystem.databinding.FragmentChannelViewBinding;
 
+/**
+ * Fragment for a specific channel. Send messages into that channel
+ */
 public class ChannelViewFragment extends Fragment {
 
+    /**
+     * Binding to access elements from the layout
+     */
+    private FragmentChannelViewBinding binding;
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        this.binding = FragmentChannelViewBinding.inflate(inflater, container, false);
+
+        //Set-Up the RecyclerView
+        RecyclerView recyclerView = this.binding.fragmentChannelViewRecyclerView;
+
+        AddMessageViewModel viewModel = new ViewModelProvider(this.requireActivity()).get(AddMessageViewModel.class);
+
+        ChannelViewContentAdapter adapter = new ChannelViewContentAdapter(viewModel.getUri().getValue());
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this.getContext());
+
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(adapter);
+
+        this.binding.fragmentChannelViewAddMessageButton.setOnClickListener(view -> {
+            Navigation.findNavController(view).navigate(R.id.action_nav_channel_view_to_nav_add_message);
+        });
+
+        //TODO: make channel name visible in app bar instead of "Channel"
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_channel_view, container, false);
+
+        return this.binding.getRoot();
     }
 }
