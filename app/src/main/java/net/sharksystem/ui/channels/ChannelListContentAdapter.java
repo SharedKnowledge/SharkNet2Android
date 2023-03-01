42a610614d1be8130980671f2ffe8e21ff85b575
Clemens <cle.zander@gmail.com>
2023-03-01 08:17:08 +0100

    Added onClickListeners for short and long clicks on channels in the channel list view. Instead of doing the logic here, the fragment is informed by subscribing as a listener for clicks on recycler view items.

--- app/src/main/java/net/sharksystem/ui/channels/ChannelListContentAdapter.java
+++ app/src/main/java/net/sharksystem/ui/channels/ChannelListContentAdapter.java
@@ -7,7 +7,6 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.RecyclerView;
 
 import net.sharksystem.R;
@@ -16,45 +15,78 @@ import net.sharksystem.messenger.SharkMessengerException;
 import net.sharksystem.sharknet.android.SharkNetApp;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+/**
+ * ContentAdapter for items in the ChannelListFragment
+ */
 public class ChannelListContentAdapter
         extends RecyclerView.Adapter<ChannelListContentAdapter.ViewHolder> {
 
+    /**
+     * Listeners which want to be informed when an item was clicked
+     */
+    private List<ChannelSelectedListener> listeners;
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
+    /**
+     * Constructor of the ContentAdapter
+     */
+    public ChannelListContentAdapter() {
+        this.listeners = new ArrayList<>();
+    }
+
+    /**
+     * ViewHolder for the items (=channels)
+     */
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        /**
+         * TextView of the channel uri
+         */
         private final TextView uriTextView;
+
+        /**
+         * TextView of the channel name
+         */
         private final TextView nameTextView;
 
+        /**
+         * Constructor of the ViewModel
+         * @param view the item as view
+         */
         public ViewHolder(View view) {
             super(view);
             // Define click listener for the ViewHolder's View
-
             this.uriTextView = view.findViewById(R.id.sn_channel_list_row_uri);
             this.nameTextView = view.findViewById(R.id.sn_channel_list_row_name);
-
-            view.setOnClickListener(itemView -> {
-                Navigation.findNavController(itemView).navigate(R.id.nav_channel_view);
-            });
         }
 
         /**
-         * Returns the TextView of the channel uri
-         * @return the channel uri TextView
+         * Returns the uri as TextView
+         * @return the uri as TextView object
          */
         public TextView getUriTextView() {
             return this.uriTextView;
         }
-
-        /**
-         * Returns the TextView of the channel name
-         * @return the channel name TextView
-         */
-        public TextView getNameTextView() {
-            return this.nameTextView;
-        }
     }
 
 
+    /**
+     * Adds a listener to the list of listeners
+     * @param listener the new listener
+     */
+    public void addChannelSelectedListener(ChannelSelectedListener listener) {
+        this.listeners.add(listener);
+    }
+
+    /**
+     * Called when a new item is created
+     * @param parent   The ViewGroup into which the new View will be added after it is bound to
+     *                 an adapter position.
+     * @param viewType The view type of the new View.
+     * @return The new ViewHolder of the item
+     */
     @NonNull
     @Override
     public ChannelListContentAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -62,10 +94,30 @@ public class ChannelListContentAdapter
         View view = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.sn_channels_list_row, parent, false);
 
-        return new ViewHolder(view);
+        ViewHolder viewHolder = new ViewHolder(view);
+
+        //inform the listeners when items were clicked.
+        view.setOnClickListener(itemView -> {
+            for (ChannelSelectedListener listener : this.listeners)
+                listener.channelSelectedWithShortClick(viewHolder);
+
+        });
+
+        view.setOnLongClickListener(itemView -> {
+            for (ChannelSelectedListener listener : this.listeners)
+                listener.channelSelectedWithLongClick(viewHolder);
+            return false;
+        });
+
+        return viewHolder;
     }
 
-
+    /**
+     * Bind ViewHolder to data
+     * @param holder   The ViewHolder which should be updated to represent the contents of the
+     *                 item at the given position in the data set.
+     * @param position The position of the item within the adapter's data set.
+     */
     @Override
     public void onBindViewHolder(@NonNull ChannelListContentAdapter.ViewHolder holder, int position) {
         try {
