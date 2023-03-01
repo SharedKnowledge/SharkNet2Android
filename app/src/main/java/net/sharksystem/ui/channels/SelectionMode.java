f0597c91f80856ac331fe63311187449d066c517
Clemens <cle.zander@gmail.com>
2023-03-01 09:11:43 +0100

    enum for mode in which a recycler view can be

---
+++ app/src/main/java/net/sharksystem/ui/channels/SelectionMode.java
@@ -0,0 +1,17 @@
+package net.sharksystem.ui.channels;
+
+/**
+ * Mode in which a fragment with a recycler view can be, if the items can be deleted
+ */
+public enum SelectionMode {
+    /**
+     * State in which a selected item redirects to a more detailed view of the item or something
+     * similar
+     */
+    SELECT,
+
+    /**
+     * State in which items are deleted
+     */
+    DELETE
+}
