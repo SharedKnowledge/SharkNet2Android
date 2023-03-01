83d668586964e8da52f5b3b853c1dc64a907d587
Clemens <cle.zander@gmail.com>
2023-03-01 09:14:04 +0100

    New test classes for tests. Test are following today. App is with its now refactored structure easier to test, as benefits from the API can be used

---
+++ app/src/androidTest/java/net/sharksystem/sharknet/ui/channel/ChannelListViewTest.java
@@ -0,0 +1,27 @@
+package net.sharksystem.sharknet.ui.channel;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+
+import net.sharksystem.SharkException;
+import net.sharksystem.messenger.android.SNChannelsListActivity;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.io.IOException;
+
+public class ChannelListViewTest {
+
+    @Rule
+    public ActivityScenarioRule<SNChannelsListActivity> activityRule =
+            new ActivityScenarioRule<>(SNChannelsListActivity.class);
+
+
+    /**
+     * This test ensures, that all components are displayed
+     */
+    @Test
+    public void channelOverviewShowsNecessaryComponents() throws SharkException, IOException {
+
+    }
+}
