b7da605b2a2e978776870a94d1f1782073402d16
Clemens <cle.zander@gmail.com>
2023-03-06 23:41:44 +0100

    changing name is now refactored

--- app/src/main/java/net/sharksystem/ui/settings/personal/PersonalDataFragment.java
+++ app/src/main/java/net/sharksystem/ui/settings/personal/PersonalDataFragment.java
@@ -2,19 +2,50 @@ package net.sharksystem.ui.settings.personal;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import net.sharksystem.R;
+import net.sharksystem.SharkException;
+import net.sharksystem.databinding.FragmentPersonalDataBinding;
+import net.sharksystem.sharknet.android.SharkNetApp;
 
 public class PersonalDataFragment extends Fragment {
+
+    private FragmentPersonalDataBinding binding;
+
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_personal_data, container, false);
+
+        this.binding = FragmentPersonalDataBinding.inflate(this.getLayoutInflater());
+
+        this.binding.fragmentPersonalDataNameInput.
+                setText(SharkNetApp.getSharkNetApp().getOwnerName());
+
+        this.binding.fragmentPersonalDataSaveButton.setOnClickListener(view ->  {
+
+            String ownerNameString = this.binding.fragmentPersonalDataNameInput.getText().toString();
+
+            SharkNetApp app = SharkNetApp.getSharkNetApp();
+            try {
+                // TODO should this be initialize system? that would change ownerID
+                app.changeOwnerName(this.requireActivity(), ownerNameString);
+            } catch (SharkException e) {
+                Toast.makeText(this.getContext(), e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+            }
+
+            Navigation.findNavController(view).
+                    navigate(R.id.action_nav_personal_data_to_nav_settings);
+        });
+
+        return this.binding.getRoot();
     }
 }
